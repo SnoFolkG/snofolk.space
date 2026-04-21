10734c223d555dd0ed18ac0e7e58bf034abe6cbb
SnoFolk <marsowob@gmail.com>
2026-04-21 22:50:27 +0300

    Delete _worker.js

--- _worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(req, env) {
-    const url = new URL(req.url);
-    if (url.pathname === "/album" && url.searchParams.has("id")) {
-      return Response.redirect(`${url.origin}/albums/${url.searchParams.get("id")}`, 301);
-    }
-    return env.ASSETS.fetch(req);
-  }
-};
