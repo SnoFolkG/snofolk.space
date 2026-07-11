4da1cdee8aa900d6a7abf9251d32dd53fbe425ab
SnoFolk <marsowob@gmail.com>
2026-07-11 18:45:19 +0300

    Collection v7.2.1 - UpdateHistory

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const DATA_URL = '/data/album.json';
 
 // Site and Collection version (semantic version strings)
 const SITE_VERSION = '4.10.3';
-const COLLECTION_VERSION = '7.2.0';
+const COLLECTION_VERSION = '7.2.1';
 
 
 let allAlbumsData = [];
