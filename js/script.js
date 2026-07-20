7d489ad5dd1debdb27da105688cc156df064ced6
SnoFolk <marsowob@gmail.com>
2026-07-20 16:22:40 +0300

    Site update v 5.0.1

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 const DATA_URL = '/data/album.json';
 
 // Site and Collection version (semantic version strings)
-const SITE_VERSION = '5.0.0';
+const SITE_VERSION = '5.0.1';
 const COLLECTION_VERSION = '7.5.0';
 
 
