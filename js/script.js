56a5a51a409e231347fa978d0df43c9467507e14
SnoFolk <marsowob@gmail.com>
2026-07-28 17:18:21 +0300

    Mistakes

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const DATA_URL = '/data/album.json';
 
 // Site and Collection version (semantic version strings)
 const SITE_VERSION = '5.3.1';
-const COLLECTION_VERSION = '7.6.0';
+const COLLECTION_VERSION = '7.7.0';
 
 
 let allAlbumsData = [];
